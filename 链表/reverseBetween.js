3464d9ba175c3c44445d8ab286d397dbe2b2f929
Thomas Z <2633745073@qq.com>
2018-11-02 11:01:56 +0800

    修改反转部分列表的写法
    修改反转部分列表的写法

--- 链表/reverseBetween.js
+++ 链表/reverseBetween.js
@@ -1,18 +1,17 @@
-// toFix
-var reverseBetween = function (head, m, n) {
-  let curr = head, pre = null, count = 1, next = null, start = null , end = null, pstart = null,pend = null
-  while (count<=n) {
-    next = curr.next
-    if (count === m-1) { start = curr; pstart = start.next }
-    if (count === n) { end = curr; pend = end.next }
-    if (count > m && count <= n) {
-        curr.next = pre
-    }
-    count ++
-    pre = curr
-    curr = next
+// 找到需要反转的链接，把最后一个放到第一个，然后改变指向
+let reverseBetween = function (head, m, n) {
+  let start = { next: head, val: null }
+  let pre = start
+  let cur = start.next
+  for (let i = 1; i < m; i++) {
+    pre = pre.next
+    cur = cur.next
   }
-  if(start) start.next = end
-  if(pstart) pstart.next = pend
-  return head
+  for (let i = 1; i <= n - m; i++) {
+    let next = cur.next
+    cur.next = next.next
+    next.next = pre.next
+    pre.next = next
+  }
+  return start.next
 }
