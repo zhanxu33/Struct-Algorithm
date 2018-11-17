4cd28906708de2eaac420205e6c6bb98ee351b2e
zhanxu33 <2633745073@qq.com>
2018-11-17 21:29:49 +0800

    添加hashTable
    添加hashTable

---
+++ Hash/HashTable.js
@@ -0,0 +1,19 @@
+const HashTable = function() {
+  this.myList = []
+}
+
+HashTable.prototype.add = function(val) {
+
+}
+
+HashTable.prototype.remove = function(val) {
+
+}
+
+HashTable.prototype.get = function(val) {
+
+}
+
+const hash = function() {
+  
+}
--- Hash/利用素数Hash.js
+++ Hash/利用素数Hash.js
@@ -18,7 +18,7 @@ var groupAnagrams = function(strs) {
     res.push(tmp[key])
   }
   return res
-};
+}
 
 let hash = function(str) {
   const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103]
