8fab931bf982231bc8d745eb449fb2afaa149704
Thomas Z <2633745073@qq.com>
2018-11-20 20:48:50 +0800

    四种遍历二叉树
    四种遍历二叉树

---
+++ 树/遍历二叉树.js
@@ -0,0 +1,35 @@
+
+function tlr (root) {
+  if (root === null) return
+  console.log(root.value)
+  tlr(root.left)
+  tlr(root.right)
+}
+
+function ltr (root) {
+  if (root === null) return
+  ltr(root.left)
+  console.log(root.value)
+  ltr(root.right)
+}
+
+function lrt (root) {
+  if (root === null) return
+  lrt(root.left)
+  lrt(root.right)
+  console.log(root.value)
+}
+
+/**
+ * 按层查找
+ * @param {*} root 
+ */
+function byFloor (root) {
+  const arr = [root]
+  while (arr.length) {
+    const first = arr.pop()
+    console.log(first.value)
+    if (first.left) arr.push(first.left)
+    if (first.right) arr.push(first.right)
+  }
+}
