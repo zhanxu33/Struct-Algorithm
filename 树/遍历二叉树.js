e4638eb46b63e67aa431a97b931f1fc052c7fbd9
zhanxu33 <2633745073@qq.com>
2018-12-01 22:03:43 +0800

    Update 遍历二叉树.js
    添加非递归遍历方法

--- 树/遍历二叉树.js
+++ 树/遍历二叉树.js
@@ -6,6 +6,26 @@ function tlr (root) {
   tlr(root.right)
 }
 
+function tlr (root) {
+  let curNode = root
+  const queue = []
+  const res = []
+  while (queue.length || curNode !== NIL) {
+
+    while (curNode !== NIL) {
+      res.push(curNode.val)
+      queue.push(curNode)
+      curNode = curNode.left
+    }
+
+    if (queue.length) {
+      curNode = queue.pop()
+      curNode = curNode.right
+    }
+  }
+  return res
+}
+
 function ltr (root) {
   if (root === null) return
   ltr(root.left)
@@ -13,11 +33,64 @@ function ltr (root) {
   ltr(root.right)
 }
 
+/**
+ * ltr排序，非递归方式
+ */
+ function ltr(root) {
+  let curNode = root
+  const queue = []
+  const res = []
+  while (queue.length || curNode !== NIL) {
+
+    while (curNode !== NIL) {
+      queue.push(curNode)
+      curNode = curNode.left
+    }
+
+    if (queue.length) {
+      curNode = queue.pop()
+      res.push(curNode.val)
+      curNode = curNode.right
+    }    
+  }
+  return res
+}
+
 function lrt (root) {
   if (root === null) return
   lrt(root.left)
   lrt(root.right)
   console.log(root.value)
+}
+
+// 非递归方式实现
+function lrt (root) {
+  let curNode = root
+  const queue = []
+  const res = []
+  while (queue.length || curNode !== NIL) {
+
+    while (curNode !== NIL) {
+      curNode.isFirst = 1
+      queue.push(curNode)
+      curNode = curNode.left
+    }
+
+    if (queue.length) {
+      curNode = queue.pop()
+
+      if (res.isFirst === 2) {
+        res.push(curNode.val)
+        curNode = NIL
+      }     
+      if (res.isFirst === 1) {
+        res.isFirst++
+        queue.push(curNode)
+        curNode = curNode.right
+      }
+    }
+  }
+  return res
 }
 
 /**
