84e5cdeaa89672476a6cd665dc7a3c0912294411
zhanxu33 <2633745073@qq.com>
2018-12-07 22:55:50 +0800

    完善RK算法
    添加算26的次方的缓存

--- 字符串匹配/RK.js
+++ 字符串匹配/RK.js
@@ -7,11 +7,16 @@
  * @param {*} main 主串
  */
 function RK (s, main) {
-  const n = main.length - s.length - 1
+  const n = main.length - s.length + 1
+  const l = s.length
   const sHash = hash(s)
-  let prev = 0
-  for (let i = 0; i < n; i++) {
-    
+  let prev = hash(main.substr(0, l))
+  if (prev === sHash) return 0
+
+  for (let i = 1; i < n; i++) {
+    let curHash = 26 * (prev - (main[i - 1].charCodeAt() - 96) * getNumber(l - 1)) + (main[i + l - 1].charCodeAt() - 96)
+    if (curHash === sHash) return i
+    prev = curHash
   }
 }
 
@@ -23,7 +28,18 @@ function hash (str) {
   let res = 0
   const length = str.length
   for (let i = 0; i < length; i++) {
-    res += (str[i] - 'a') * 26 ^ (length - i - 1)
+    res += (str[i].charCodeAt() - 96) * 26 ** (length - i - 1)
   }
   return res
+}
+
+/**
+ * 存储26次方的数字
+ */
+const cacheNumber = []
+function getNumber(n) {
+  if (!cacheNumber[n]) {
+     cacheNumber[n] = 26 ** n
+  }
+  return cacheNumber[n]
 }
