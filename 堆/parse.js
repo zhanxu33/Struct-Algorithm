68e0d1f1180b0ec0024e78c714598c644bdaa570
Thomas Z <2633745073@qq.com>
2018-12-12 20:55:19 +0800

    字符串转为数组
    字符串转为数组

--- 堆/parse.js
+++ 堆/parse.js
@@ -7,25 +7,55 @@
  * parse(`123`) // null
  * parse(`(1,2`) // null
  * parse(`()()`) // null
- * @param {*} str 
+ * 字符串转为数组
  */
 
 function parse (str) {
-  let res = []
-  let index = 0
+  if (str[0] !== '(') return null
+  str = str.replace(/\s/g, '')
+  let leftStack = []
+  let isString = false
+  let curItem = ''
+  let curArrObj = {
+    arr: undefined,
+    parent: undefined
+  }
   for (let s of str) {
     switch (s) {
       case '(':
-        
+        let temp = []
+        if (curArrObj.arr && leftStack.length < 1) return null
+        if (curArrObj.arr) curArrObj.arr.push(temp)
+        else curArrObj.arr = temp
+        curArrObj = {
+          arr: temp,
+          parent: curArrObj
+        }
+        leftStack.push(curArrObj)
         break
       case '\'':
-        break
-      case '\'':
+        isString = !isString
         break
       case ')':
+        if (leftStack.length < 1) return null
+        if (curItem !== '') curArrObj.arr.push(curItem)
+        curArrObj = curArrObj.parent
+        leftStack.pop()
+        curItem = ''
+        break
+      case ',':
+        if (curItem !== '') curArrObj.arr.push(curItem)
+        curItem = ''
         break
       default:
+        if (isString) {
+          curItem += s
+        } else {
+          curItem = Number(s)
+          if (isNaN(curItem)) return null
+        }
     }
   }
-  return res
+  if (leftStack.length > 0) return null
+  return curArrObj.arr
 }
