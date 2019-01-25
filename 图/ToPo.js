ce661269f534a3151c8faf526c0b3482556ffe9f
Thomas Z <2633745073@qq.com>
2019-01-25 17:19:12 +0800

    拓扑排序
    使用图记录多个之间的关系，巧用邻接链表记录依赖，使用数量记录依赖数而不是真实操作

--- 图/ToPo.js
+++ 图/ToPo.js
@@ -4,11 +4,47 @@
  * 2. 统计每个元素的入度，即是他所有依赖的对象
  * 3. 如果入度为0，即可把它输出了
  */
+
 class ToPo {
-  constructor(arr) {
-
+  constructor(arrs) {
+    this.toPo = {}
+    arrs.forEach(([key, value]) => {
+      const target = this.toPo[key]
+      if (target) {
+        target.arr.push(value)
+        target.count++
+      } else {
+        this.toPo[key] = { arr: [value], count: 1 }
+      }
+    })
   }
-  sort () {
 
+  sort() {
+    const queue = []
+    const res = []
+    const keys = Object.keys(this.toPo)
+    keys.forEach((key) => {
+      const current = this.toPo[key]
+      if (current.count === 0) queue.push(key)
+    })
+
+    while (queue.length) {
+      const head = queue.shift()
+      res.push(head)
+
+      keys.forEach((key) => {
+        const curr = this.toPo[key]
+        const arr = curr.arr
+        const count = curr.count
+
+        if (count < 1) return
+        if (arr.findIndex(x => x === head) === -1) return
+
+        const newCount = --curr.count
+        if (newCount < 1) queue.push(key)
+      })
+    }
+
+    return res
   }
 }
