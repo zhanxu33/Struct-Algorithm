9dca0bded663806af18b1e8f773f0b17c1c85afa
Thomas Z <2633745073@qq.com>
2019-01-30 11:08:33 +0800

    dijkstra
    记录最短路径

--- 图/dijkstra.js
+++ 图/dijkstra.js
@@ -1,6 +1,6 @@
 /**
  * dijkstra是单源最短路径搜索，就是一个点到另一个点的路径
- * 1.维护一个2维数组，邻接矩阵
+ * 1.维护一个2维数组-邻接矩阵，来表示点与点之间的关系
  * 2.维护一个数组，是一个点到其余几个点的最短路径
  * 3.每次找当前可达的最短路径，确定相邻的最短长度，然后将值更新到最短路径数组里面
  */
@@ -11,6 +11,7 @@
  */
 function dijkstra (arr) {
   const dist = arr[0]
+  const predecessor = [0]
   const queue = [{ value: dist, index: 0 }]
 
   while (queue.length) {
@@ -22,14 +23,17 @@ function dijkstra (arr) {
     p.forEach((value, index) => {
       if (typeof value !== 'undefined') {
         if (value < min.value) min = { value, index }
-        
+
         const curValue = dist[curIndex] + value
-        if (curValue < dist[index]) dist[index] = curValue
+        if (curValue < dist[index]) {
+          dist[index] = curValue
+          predecessor[index] = curIndex
+        }
       }
     })
 
     if (min.index !== -1) queue.push({ value: arr[min.index], index: min.index })
   }
 
-  return dist
+  return { dist, predecessor }
 }
